5c7b5a943e44c1749c739ebb73e6bb8883b5afff
AntonM-248 <53975792+AntonM-248@users.noreply.github.com>
2021-04-23 08:26:51 -0400

    Add project files.

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //string todaysDate = DateTime.Now.ToShortDateString();
+            //return Ok(todaysDate);
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Ok($"Entered the id: {id}");
+        }
+    }
+}
---
+++ InAndOut/Models/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Item Name")]
+        [Required]
+        public string ItemName { get; set; }
+        [DisplayName("Price")]
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price should be > 0.01")]
+        public double Price { get; set; }
+    }
+}
